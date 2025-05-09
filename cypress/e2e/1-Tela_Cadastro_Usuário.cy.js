819b80aac5288b0c494a6d76c1d3fe5b6c68f72a
marcelolsilva27 <marcelo.lsilva27@gmail.com>
2025-05-08 22:59:20 -0300

    Alteração tela cadastro

--- cypress/e2e/1-Tela_Cadastro_Usuário.cy.js
+++ cypress/e2e/1-Tela_Cadastro_Usuário.cy.js
@@ -1,4 +1,4 @@
-///Script de teste que deve criar um novo usuário que não esteja cadastrado
+///Script de teste que deve criar um novo usuário que não esteja cadastrado no E-commerce DSJ
 // Resultado esperado: O Sistema  deve criar o novo usuário 
 
 describe('Tela Cadastro de Usuário', () => {
@@ -33,37 +33,30 @@ describe('Tela Cadastro de Usuário', () => {
       cy.wait(2000);
       //data de aniversário
       cy.get('#birthDate').click({ force: true }).type('1982-07-27', { force: true });
-
       //Selecionar Gênero
-      
       cy.get('#gender').click({ force: true }).type('Masculino', { force: true });
-      
       //cy.get('#gender').first().click();
       cy.wait(2000); 
-       
       cy.get('#email').click().type('marcelo.lsilva27@gmail.com', { force: true });
       cy.wait(2000);
       cy.get('#phone').click().type('47991396138');
       cy.wait(2000);
       cy.get('#landlinePhone').click().type('47991396139');
       cy.wait(2000);
-      
       cy.get('#password').click().type('Marcelo@2707'); 
       cy.wait(2000);
-
       cy.get('#confirmPassword').click().type('Marcelo@2707'); 
       cy.wait(2000);
       // Fleg para receber notificações
       cy.get('#notifyByEmail').click();
       cy.get('#notifyByWhatsapp').click();
       cy.get('#receivePromotions').click();
-
       cy.get('.space-y-5 > .inline-flex').click();
       cy.wait(2000);
     })
       
     // Esse teste valida que esse usuário já possui cadastro no E-commerce
-    it.skip('Validação de Usuário ja Cadastrado', () => {
+    it('Validação de Usuário ja Cadastrado', () => {
             cy.visit('https://homolog-shop-ecommerce.szsolucoes.com.br/'); 
             cy.get('.justify-center > .text-base').click();
             cy.wait(2000);
@@ -97,7 +90,7 @@ describe('Tela Cadastro de Usuário', () => {
     
    
 
-    it.skip('Validação do CPF', () => {
+    it('Validação do CPF', () => {
         cy.visit('https://homolog-shop-ecommerce.szsolucoes.com.br/'); 
         cy.get('.justify-center > .text-base').click();
       // Criar conta
@@ -128,20 +121,17 @@ describe('Tela Cadastro de Usuário', () => {
    
  
 
-    it.skip('Validação do Formato de E-mail', () => {
+    it('Validação do Formato de E-mail', () => {
         cy.visit('https://homolog-shop-ecommerce.szsolucoes.com.br/'); 
         cy.get('.justify-center > .text-base').click();
       // Criar conta
         cy.wait(2000);
         cy.get('div.text-sm > a').click();
-    
         cy.wait(2000);
         cy.get('#firstName').click().type('Marcelo Luiz');
         cy.wait(2000);
         cy.get('#lastName').click().type('da Silva');
         cy.wait(2000);
-  
-      
         cy.get('#cpfOrCnpj').click().type('428.383.450-52');
         cy.wait(2000);
         cy.get('#phone').click().type('47991357896');
@@ -158,21 +148,17 @@ describe('Tela Cadastro de Usuário', () => {
     })
     
   
-
     it.skip('Formato da Senha Inválido', () => {
         cy.visit('https://homolog-shop-ecommerce.szsolucoes.com.br/'); 
         cy.get('.justify-center > .text-base').click();
       // Criar conta
         cy.wait(2000);
         cy.get('div.text-sm > a').click();
-    
-   
         cy.wait(2000);
         cy.get('#firstName').click().type('Marcelo Luiz');
         cy.wait(2000);
         cy.get('#lastName').click().type('da Silva');
         cy.wait(2000);
-  
         //061.142.960-87
         cy.get('#cpfOrCnpj').click().type('486.543.970-63');
         cy.wait(2000);
